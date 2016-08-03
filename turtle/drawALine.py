3420f32e807feb303fd7be5845c69ea4d116fa1d
Woo, Jasmine <Jasmine_Woo@intuit.com>
2016-08-03 14:53:24 -0600

    draw a line and square tutorials

---
+++ turtle/drawALine.py
@@ -0,0 +1,20 @@
+import turtle       # allows us to use the turtles library
+
+
+window = turtle.Screen()    # creates a graphics window
+bradley = turtle.Turtle()  # create a turtle named bradley
+
+# Exit when you click the turtle graphics window
+def exit ():
+    window.exitonclick()
+
+
+#########################################################
+
+# Draw a line!
+
+bradley.forward(100)    # tell bradley to move forward by 100 units
+
+# Exit the program
+
+exit()
