5a857c31259b527c4e877533995f2d8c62ba49d6
Parsana Pillay <parsana_pillay@intuit.com>
2016-08-03 16:02:54 -0600

    add python intro

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # codeCamp
 
+# following steps to clone and start tutorial
 # export GIT_SSL_NO_VERIFY=1
 # git clone git@github.com/parsana/codeCamp.git
+# idle3 intro/intro.py
---
+++ intro/intro.py
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD>
+<TITLE>Moved Permanently</TITLE>
+</HEAD>
+<BODY BGCOLOR="#FFFFFF" TEXT="#000000">
+<H1>Moved Permanently</H1>
+The document has moved <A HREF="https://raw.githubusercontent.com/raspberrypilearning/python-intro/master/code/intro.py">here</A>.
+</BODY>
+</HTML>
