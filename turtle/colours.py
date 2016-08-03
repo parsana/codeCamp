6d9a6d8158324cb408cbd725964430b565b647fb
Woo, Jasmine <Jasmine_Woo@intuit.com>
2016-08-03 15:16:56 -0600

    colors tutorial

---
+++ turtle/colours.py
@@ -0,0 +1,36 @@
+import turtle
+
+window = turtle.Screen()    # creates a graphics window
+bradley = turtle.Turtle()   # create a turtle named bradley
+bradley.shape("turtle")     # transform bradley into turtle form
+window.colormode(255)       # set color mode
+
+# Exit when you click the turtle graphics window
+def exit ():
+    window.exitonclick()
+
+
+##################################################################
+# The default colour for the pen used by the turtle cursor is black,
+# and the default background colour is white. You can change the colours
+# to make your shapes look even better.
+
+# The code below contains three variables called R, G, and B.
+
+R = 255
+G = 255
+B = 0
+
+window.bgcolor(R, G, B)
+
+# Variables are a way of storing a value and giving it a name.
+# For instance, there is a variable name R with a value of 255.
+# Try changing the values of the three variables, and see what happens.
+# (Note: the maximum value is 255, and after this there will be no effect.)
+
+
+# Turtles can change color too!
+# Declare 3 variables and use the color() function to change Bradley's color
+# [insert code below]
+
+exit()
