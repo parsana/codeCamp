c62615f519b1d1307eb5b76d440a95eb6c403efb
Woo, Jasmine <Jasmine_Woo@intuit.com>
2016-08-04 09:39:29 -0600

    solutions

---
+++ turtle/solutions/lesson_2_sol.py
@@ -0,0 +1,31 @@
+import turtle               # allows us to use the turtles library
+
+
+window = turtle.Screen()    # creates a graphics window
+bradley = turtle.Turtle()   # create a turtle named bradley
+bradley.shape("turtle")     # transform bradley into turtle form
+
+# Exit when you click the turtle graphics window
+def exit ():
+    window.exitonclick()
+
+
+#########################################################
+
+# Now let's try making the turtle turn around.
+# To do this you need to instruct the turtle not only to move forward,
+# but also to turn right or left.
+
+bradley.forward(100)
+bradley.right(90)
+bradley.forward(100)
+
+# Let's try to draw a square!
+
+bradley.right(90)
+bradley.forward(100)
+bradley.right(90)
+bradley.forward(100)
+
+# Exit the program
+exit()
---
+++ turtle/solutions/lesson_5_sol.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import turtle
+
+window = turtle.Screen()    # creates a graphics window
+bradley = turtle.Turtle()   # create a turtle named bradley
+bradley.shape("turtle")     # transform bradley into turtle form
+window.colormode(255)       # set color mode
+
+# Exit when you click the turtle graphics window
+def exit ():
+    window.exitonclick()
+
+###################################################################
+
+# The if-then statement is the most basic of all the control flow statements.
+# It tells your program to execute a certain section of code only if
+# a particular test evaluates to true.
+
+# if (a condition evaluates to True):
+#       then do these things only for ‘True’
+# else:
+#       otherwise do these things only for ‘False’.
+
+
+#direction = raw_input("Go left or right? ")
+
+#if direction == "left":
+#    bradley.left(60)
+#    bradley.forward(50)
+#    exit()
+#if direction == "right":
+#    bradley.right(60)
+#    bradley.forward(50)
+#    exit()
+    
+#################################################################
+
+# Let's try coding an if statment to either draw a square or a triangle
+
+shape = raw_input("square or triangle? ")
+
+if shape == "square":
+    for i in range(4):
+        bradley.forward(100)
+        bradley.right(90)
+    exit()
+if shape == "triangle":
+    for i in range(3):
+        bradley.forward(320)
+        bradley.left(120)
+    exit()
+
+
+
+
